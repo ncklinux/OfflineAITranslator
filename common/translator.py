64b1693dfb0c9609e6a9a6b91f53de8f5ca25876
Nick <68464552+ncklinux@users.noreply.github.com>
2024-06-15 13:02:27 +0200

    feat: add type annotations and linting

--- common/translator.py
+++ common/translator.py
@@ -2,22 +2,18 @@ from transformers import MarianMTModel, MarianTokenizer
 
 
 class Translator:
-    def __init__(self, source_language, target_language, text):
+    def __init__(self, src_lang: str, trg_lang: str, text: str):
         super(Translator, self).__init__()
-        self.source_language = source_language
-        self.target_language = target_language
+        self.src_lang = src_lang
+        self.trg_lang = trg_lang
         self.text = text
 
-    def set_translate(self):
-        model_name = f"opus-mt-{self.source_language}-{self.target_language}"
-        tokenizer = MarianTokenizer.from_pretrained(f"./language_models/{model_name}")
-        pretrained_model = MarianMTModel.from_pretrained(
-            f"./language_models/{model_name}"
+    def set_translate(self) -> str:
+        model = f"opus-mt-{self.src_lang}-{self.trg_lang}"
+        token = MarianTokenizer.from_pretrained(f"./language_models/{model}")
+        trained = MarianMTModel.from_pretrained(f"./language_models/{model}")
+        generate = trained.generate(
+            **token(self.text, return_tensors="pt", padding=True)
         )
-        translated = pretrained_model.generate(
-            **tokenizer(self.text, return_tensors="pt", padding=True)
-        )
-        translation = [
-            tokenizer.decode(t, skip_special_tokens=True) for t in translated
-        ]
-        return str(translation).strip("['']")
+        result = [token.decode(t, skip_special_tokens=True) for t in generate]
+        return str(result).strip("['']")
